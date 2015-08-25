0b44f2115f9371c922e90f906087ddf05c732280
Stuart Nel <Snel@planittesting.com>
2015-08-25 17:16:17 +1000

    Closing off the day - 25/08

--- TrainingProject/src/pageModels/BasePage.java
+++ TrainingProject/src/pageModels/BasePage.java
@@ -7,15 +7,10 @@ public abstract class BasePage {
 	static protected WebDriver d;
 	
 	public BasePage(WebDriver driver) 
-	
 	{
-	
 	d = driver;
-	
 	d.switchTo().defaultContent();
-	
 	d.manage().timeouts().setScriptTimeout(60, null);
-	
 	}
 
 }
