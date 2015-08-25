34e309344a6b76d021fceacd202bd45f0d39edd2
Stuart Nel <Snel@planittesting.com>
2015-08-25 15:14:36 +1000

    Script BasePage class

--- TrainingProject/src/pageModels/BasePage.java
+++ TrainingProject/src/pageModels/BasePage.java
@@ -2,4 +2,25 @@ package pageModels;
 
 public class BasePage {
 
+	package automationFramework.pageModels;
+
+
+	import org.openqa.selenium.WebDriver;
+
+	public abstract class BasePage {
+
+	static protected WebDriver d;
+
+	public BasePage(WebDriver driver) 
+
+	{
+
+	d = driver;
+
+	d.switchTo().defaultContent();
+
+	d.manage().timeouts().setScriptTimeout(60, null);
+
+	}
+
 }
