8647ee0714d12d3a946bba5a5de051d5e3aca646
Stuart Nel <Snel@planittesting.com>
2015-08-25 10:05:03 +1000

    Placed all Search By values at the beginning of the class.

--- TrainingProject/src/automationFramework/FirstTestCase.java
+++ TrainingProject/src/automationFramework/FirstTestCase.java
@@ -12,48 +12,43 @@ public class FirstTestCase {
 
    public static void main(String[] args) {
 
+       //Search Criteria
+       By MyAccount = By.xpath(".//*[@id='account']/a");
+       By Username = By.id("log");
+       By Password = By.id("pwd");
+       By Login = By.id("login");
+       By Logout = By.xpath(".//*[@id='account_logout']/a");     
+       
        // Create a new instance of the Firefox driver
-
        driver = new FirefoxDriver();
 
        //Put a Implicit wait, this means that any search for elements on the page could take the time the implicit wait is set for before throwing exception
-
        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
        //Launch the Online Store Website
-
        driver.get("http://www.store.demoqa.com");
 
        // Find the element that's ID attribute is 'account'(My Account) 
-
-       driver.findElement(By.xpath(".//*[@id='account']/a")).click();
+       driver.findElement(MyAccount).click();
 
        // Find the element that's ID attribute is 'log' (Username)
-
        // Enter Username on the element found by above desc.
-
-       driver.findElement(By.id("log")).sendKeys("testuser_1"); 
+       driver.findElement(Username).sendKeys("testuser_1"); 
 
        // Find the element that's ID attribute is 'pwd' (Password)
-
        // Enter Password on the element found by the above desc.
-
-       driver.findElement(By.id("pwd")).sendKeys("Test@123");
+       driver.findElement(Password).sendKeys("Test@123");
 
        // Now submit the form. WebDriver will find the form for us from the element 
-
-       driver.findElement(By.id("login")).click();
+       driver.findElement(Login).click();
 
        // Print a Log In message to the screen
-
        System.out.println(" Login Successfully, now it is the time to Log Off buddy.");
-
+       
        // Find the element that's ID attribute is 'account_logout' (Log Out)
-
-       driver.findElement (By.xpath(".//*[@id='account_logout']/a")).click();
+       driver.findElement (Logout).click();
 
        // Close the driver
-
        driver.quit();
 
            }
