e08c255ebf684c522f9bc8f7c83b6307d926c848
Stuart Nel <Snel@planittesting.com>
2015-08-25 11:29:51 +1000

    Updated UICommom class

--- TrainingProject/src/common/UICommon.java
+++ TrainingProject/src/common/UICommon.java
@@ -1,5 +1,41 @@
 package common;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class UICommon {
 
+    public static WebElement GetElement(By searchBy, WebDriver d)
+
+{
+
+WebElement elem = d.findElement(searchBy);
+
+return elem;
+
 }
+
+ 
+
+public static void ClickElement(By searchBy, WebDriver d)
+
+{
+
+WebElement elem = GetElement(searchBy, d);
+
+elem.click();
+
+}
+
+ 
+
+public static void setElement(String value, By searchBy, WebDriver d)
+
+{
+
+WebElement elem = GetElement(searchBy, d);
+
+elem.sendKeys(value);
+
+}}
