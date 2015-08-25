7cbb34b692ebadd335b4bbe549b4b1a960fe473c
Stuart Nel <Snel@planittesting.com>
2015-08-25 14:21:59 +1000

    Amended methods in FirstTestCase class to use methods in UICommon class

--- TrainingProject/src/automationFramework/FirstTestCase.java
+++ TrainingProject/src/automationFramework/FirstTestCase.java
@@ -28,26 +28,32 @@ public class FirstTestCase {
        //Launch the Online Store Website
        driver.get("http://www.store.demoqa.com");
 
-	// Find the element that's ID attribute is 'account'(My Account) 
-       driver.findElement(MyAccount).click();
+       // Find the element that's ID attribute is 'account'(My Account) 
+       //driver.findElement(MyAccount).click();
        UICommon.ClickElement(MyAccount, driver);
        
        // Find the element that's ID attribute is 'log' (Username)
        // Enter Username on the element found by above desc.
-       driver.findElement(Username).sendKeys("testuser_1"); 
+       //driver.findElement(Username).sendKeys("testuser_1");
+       UICommon.setElement("testuser_1", Username, driver);
 
+       
        // Find the element that's ID attribute is 'pwd' (Password)
        // Enter Password on the element found by the above desc.
-       driver.findElement(Password).sendKeys("Test@123");
+       //driver.findElement(Password).sendKeys("Test@123");
+       UICommon.setElement("Test@123", Password, driver);
 
        // Now submit the form. WebDriver will find the form for us from the element 
-       driver.findElement(Login).click();
+       //driver.findElement(Login).click();
+       UICommon.ClickElement(Login, driver);
+
 
        // Print a Log In message to the screen
        System.out.println(" Login Successfully, now it is the time to Log Off buddy.");
        
        // Find the element that's ID attribute is 'account_logout' (Log Out)
-       driver.findElement (Logout).click();
+       //driver.findElement (Logout).click();
+       UICommon.ClickElement(Logout, driver);
 
        // Close the driver
        driver.quit();
