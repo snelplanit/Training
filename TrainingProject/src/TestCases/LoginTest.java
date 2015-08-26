1bd0a912b7e992e08de46ca7f9d33f9e3650da2c
Stuart Nel <Snel@planittesting.com>
2015-08-27 09:15:21 +1000

    Moved script from FirstTestCase to LoginTest

--- TrainingProject/src/TestCases/LoginTest.java
+++ TrainingProject/src/TestCases/LoginTest.java
@@ -2,13 +2,41 @@ package TestCases;
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import pageModels.HomePage;
 
 public class LoginTest {
 
+	private static WebDriver driver = null;
 	@Test
 	public void test() {
 		fail("Not yet implemented");
-	}
+		       
+		   driver = new FirefoxDriver();
 
+		   driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		   driver.get("http://www.store.demoqa.com");
+
+		   HomePage homepage = new HomePage(driver);  //This instantiates the homepage (only done once)
+		      
+		   homepage.ClickMyAccountButton();
+		   homepage.TypeMyUserName("testuser_1");
+		   homepage.TypeMyPassword("Test@123");       
+		   homepage.ClickLoginButton();
+		       
+		   // Print a Log In message to the screen
+		   System.out.println(" Login Successfully, now it is the time to Log Off buddy.");
+
+		   homepage.ClickLogoutButton();
+
+		   // Close the driver
+		   driver.quit();
+
+	}
 }
