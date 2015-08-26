207cdc41ab8bd95b4c98aede1e04eb1a0e09047a
Stuart Nel <Snel@planittesting.com>
2015-08-26 15:02:45 +1000

    Did not upload previously

--- TrainingProject/src/pageModels/HomePage.java
+++ TrainingProject/src/pageModels/HomePage.java
@@ -18,7 +18,31 @@ public class HomePage extends BasePage
 		UICommon.ClickElement(MyAccount, d);
 	}
 	
+	public void TypeMyUserName()
+	{
+		UICommon.setElement("testuser_1", Username, d);
+	}
+	
+	public void TypeMyPassword()
+	{
+		UICommon.setElement("Test@123", Password, d);
+	}
+	
+	public void ClickLoginButton()
+	{
+		UICommon.ClickElement(Login, d);
+	}
+	
+	public void ClickLogoutButton()
+	{
+		UICommon.ClickElement(Logout, d);
+	}
+	
 	//Search Criteria
     By MyAccount = By.xpath(".//*[@id='account']/a");	
- 
+    By Username = By.id("log");
+    By Password = By.id("pwd");
+    By Login = By.id("login");
+    By Logout = By.xpath(".//*[@id='account_logout']/a");
+    
 }
--- TrainingProject/src/pageModels/NextPage.java
+++
@@ -1,5 +0,0 @@
-package pageModels;
-
-public class NextPage {
-
-}
