ce6554a0e25edef5c6d9a644288b9bdff994614c
Stuart Nel <Snel@planittesting.com>
2015-08-27 14:13:29 +1000

    Updating BaseTest - driver error....

--- TrainingProject/src/TestCases/BaseTest.java
+++ TrainingProject/src/TestCases/BaseTest.java
@@ -1,16 +1,25 @@
 package TestCases;
 
+import java.util.concurrent.TimeUnit;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseTest {
 
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUpBeforeClass() throws Exception 
+	{
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://www.store.demoqa.com");
 	}
-
+		
 	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+	public static void tearDownAfterClass() throws Exception 
+	{
+		// Close the driver
+		driver.quit();
 	}
 
 }
